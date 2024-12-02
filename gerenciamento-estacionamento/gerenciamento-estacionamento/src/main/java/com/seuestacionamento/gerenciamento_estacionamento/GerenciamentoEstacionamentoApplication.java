8b10df2dbd8780350af5f475253d29683bb656a0
felipeiop2 <felipeiop999@gmail.com>
2024-12-01 22:00:49 -0300

    yuri

---
+++ gerenciamento-estacionamento/gerenciamento-estacionamento/src/main/java/com/seuestacionamento/gerenciamento_estacionamento/GerenciamentoEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.seuestacionamento.gerenciamento_estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ gerenciamento-estacionamento/gerenciamento-estacionamento/src/test/java/com/seuestacionamento/gerenciamento_estacionamento/GerenciamentoEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seuestacionamento.gerenciamento_estacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
